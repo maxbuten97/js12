7e7d766e4c0fd39b418cb45fed18a9471f92ae2d
Maxim <maxbuten97@yandex.ru>
2022-01-27 17:44:00 +0700

    js15

--- js/script12.js
+++ js/script12.js
@@ -1,7 +1,26 @@
 'use strict';
+ 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+ const a = prompt('Один из последних просмотренных фильмов?', ''),
+       b = prompt('На сколько оцените его?', ''),
+       c = prompt('Один из последних просмотренных фильмов?', ''),
+       d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB); 
 
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+/* const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
  count: numberOfFilms,
@@ -20,5 +39,5 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
-
+  */
 
