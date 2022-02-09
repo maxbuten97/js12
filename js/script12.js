a1e9364cbf07b26e297fa56545d05eb4beae8c4a
Maxim <maxbuten97@yandex.ru>
2022-02-09 14:49:00 +0700

    add Alex

--- js/script12.js
+++ js/script12.js
@@ -1,6 +1,69 @@
 'use strict';
  
-/* const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+/* ФУНКЦИИ
+ let num = 20;
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10;
+    console.log(num);
+}
+
+showFirstMessage("Hello World");
+console.log(num);
+
+/ function calc(a, b) {
+return (a + b);
+}
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6)); 
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log (anotherNum);
+
+const logger = function() {
+console.log('Hello');
+};
+ logger();
+
+ const calc = (a, b) => {
+    console.log('1')
+return a + b;
+}; */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ЦИКЛЫ
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,16 +73,38 @@ const personalMovieDB = {
     privat: false
 };
 
- const a = prompt('Один из последних просмотренных фильмов?', ''),
-       b = prompt('На сколько оцените его?', ''),
-       c = prompt('Один из последних просмотренных фильмов?', ''),
-       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-console.log(personalMovieDB); 
- */
+if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+} else {
+    console.log('error');
+    i--;
+    }
+}
+ if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+ } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+     console.log("Вы классический зритель");
+ } else if (personalMovieDB.count >= 30) {
+     console.log("Вы киноман");
+ } else {
+     console.log("Ошибка");
+ }
+
+ console.log(personalMovieDB);   */
+
+
+
+
+
+
+
+ 
 
 
 /* if (4==9) {
@@ -42,7 +127,7 @@ const num = 50;
 (num === 50) ? console.log('Ok!') : console.log('Error'); */
 
 
-/* const num = 50;
+ const num = 50;
 switch (num) {
     case 49:
         console.log('Неверно');
@@ -56,9 +141,9 @@ switch (num) {
     default:
         console.log('Не в этот раз');
         break;
-} */
+} 
 
-let num = 50;
+/* let num = 50;
 
 while (num <= 55) {
     console.log(num);
@@ -76,7 +161,7 @@ while (num < 55);
 for (let i = 1; i < 8; i++ ) {
     if (i===6) {
         /* break; */
-        continue;
+/*         continue;
     }
     console.log(i);
-}
+} */ 
