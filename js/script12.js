c244ce03a659581a41dcfe3131ddab30e9c83326
Maxim <maxbuten97@yandex.ru>
2022-02-10 10:08:29 +0700

    script666

--- js/script12.js
+++ js/script12.js
@@ -8,9 +8,24 @@ const str = "test";
 /*  console.log(str.toUpperCase()); */
  console.log(str.toLowerCase());
  console.log(str); 
+let fruit = "Some fruit";
 
+console.log(fruit.indexOf("fruit"));
 
+const logg = "Hello world";
 
+/* console.log(logg.slice(6, 11)); */
+
+/* console.log(logg.substring(6, 11)); */
+
+console.log(logg.substr(6, 4));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+/* console.log(parseInt(test)); */
+console.log(parseFloat(test));
 
 
 /* ФУНКЦИИ
