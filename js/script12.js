7981304ba8416cdb1c37e893089af49aefc962ab
Maxim <maxbuten97@yandex.ru>
2022-02-11 16:31:00 +0700

    lesson 19-24

--- js/script12.js
+++ js/script12.js
@@ -1,5 +1,240 @@
 'use strict';
- 
+
+
+
+//Передача по ссылке или по значению оператор SPREAD
+
+ /* let a = 5,
+    b = a;
+
+b = b + 5;
+console.log(b);
+console.log(a);
+
+const obj = {
+    a: 5,
+    b: 1
+};
+
+const copy = obj; //Передает ссылку на объект
+copy.a = 10;
+console.log(copy);
+console.log( obj); 
+
+function copy(mainObj) {
+let objCopy = {};
+
+let key;
+for (key in mainObj) {
+    objCopy [key] = mainObj [key];
+   }
+
+   return objCopy;
+}
+
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+
+console.log(newNumbers);
+console.log(numbers);
+
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+const clone = Object.assign({}, add);
+clone.d = 20;
+ console.log(add);
+console.log(clone); 
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+newArray[1] = 'assdad';
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk','facebook'];
+      
+console.log(internet)
+
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+
+const array = ['a', 'b'];
+
+const newAarray = [...array];
+
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q}; 
+*/
+
+
+
+
+
+
+
+ // Массивы
+ /* const arr = [2, 13, 26, 8, 10];
+ arr.sort(compareNum);
+ console.log(arr);
+
+ function compareNum(a, b) {
+     return a - b; 
+ }
+/* arr[99] = 0;
+console.log(arr.length);
+console.log(arr); */
+
+/* arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} внутри массива ${arr}`);
+}); */
+
+//arr.pop();
+//arr.push(10);
+
+//console.log(arr);
+
+/* for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+} */
+
+/* for ( let value of arr) {
+    console.log(value);
+}
+ */
+/* const str = prompt("", "");
+const products = str.split(", ");
+products.sort();
+
+console.log(products.join('; ')); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ОБЪЕКТЫ
+
+/*  const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
+    }
+};
+
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+ */
+
+
+
+
+
+/* console.log(Object.keys(options).length); */ 
+
+/* console.log(options.name);
+
+delete options.name;
+
+console.log(options); */
+/* let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+    for ( let i in options[key]) {
+        console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        counter++;
+        }
+    } else {
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    counter++;
+    }
+
+}    
+console.log(counter);
+ */
+
+
+
+
+
+
+
+
+
+
+
+//Callback функции
+
+/* function first () {
+    //Do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback){
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log ('Я прошел этот урок!');
+}
+
+learnJS('JavaScript', done); */
+
 
 
 
@@ -53,8 +288,7 @@ return a + b;
 
 /*  ЦИКЛЫ и правктическое занятие */
 
-
- let numberOfFilms;
+ /* let numberOfFilms;
 
 function start() {
     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
@@ -91,10 +325,10 @@ function rememberMyFilms() {
     }
 }
 
-/* rememberMyFilms(); */
+ rememberMyFilms(); 
 
  
- function detectPersonalLevel() {
+  function detectPersonalLevel() {
     if (personalMovieDB.count < 10) {
         console.log("Просмотрено довольно мало фильмов");
      } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
@@ -106,9 +340,9 @@ function rememberMyFilms() {
      }
     
  }
-/* detectPersonalLevel(); */
+ detectPersonalLevel(); 
 
-function showMyDB (hidden) {
+ function showMyDB (hidden) {
     if(!hidden) {
       console.log(personalMovieDB);
     }
@@ -122,7 +356,7 @@ function writeYourGenres () {
 
 }
 writeYourGenres();
- 
+    */
 
 
 
